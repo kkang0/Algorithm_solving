c4e7422aa4df36e7c9c4b7ea5b4f55115a259f39
Jeong yoon Kang <124678039+kkang0@users.noreply.github.com>
2024-07-16 16:52:15 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+    string s;
+    int i;
+    cin >> s;
+    cin >> i;
+
+    cout << s[i - 1] << endl;
+
+    return 0;
+}
