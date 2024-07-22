14fadf74c999a26bbb428780a0cf8bb9791f9aad
Jeong yoon Kang <124678039+kkang0@users.noreply.github.com>
2024-07-22 12:25:00 +0900

    [level 0] Title: 이차원 배열 대각선 순회하기, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> board, int k) {
+    int sum = 0;
+    for(int i = 0; i < board.size(); i++) {
+        for(int j = 0; j < board[i].size(); j++) {
+            if(i + j <= k) sum += board[i][j];
+        }
+    }
+    
+    return sum;
+}
