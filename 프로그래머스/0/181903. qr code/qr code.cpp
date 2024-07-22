8aded91231f42f8b5107e4d829df34433b56729f
Jeong yoon Kang <124678039+kkang0@users.noreply.github.com>
2024-07-22 16:55:41 +0900

    [level 0] Title: qr code, Time: 0.02 ms, Memory: 4.03 MB -BaekjoonHub

---
+++ 프로그래머스/0/181903. qr code/qr code.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int q, int r, string code) {
+    string s;
+    for(int i = 0; i < code.size(); i++) {
+        if(i % q == r) s += code[i];
+    }
+    
+    return s;
+}
