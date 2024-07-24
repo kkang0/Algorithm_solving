248f030d7aab77bbf34852ba2b500babaa1cdbce
Jeong yoon Kang <124678039+kkang0@users.noreply.github.com>
2024-07-24 12:42:01 +0900

    [level 1] Title: 폰켓몬, Time: 0.62 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <unordered_set>
+#include <iostream>
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    unordered_set<int> s(nums.begin(), nums.end());
+    
+    return min(nums.size() / 2, s.size());
+}
