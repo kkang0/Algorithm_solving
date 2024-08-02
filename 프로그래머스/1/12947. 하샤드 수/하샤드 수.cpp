3644ab2177a6d83f142f6f9a1b73a307434cdaa7
Jeong yoon Kang <124678039+kkang0@users.noreply.github.com>
2024-08-02 15:20:14 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    string s = to_string(x);
+    int sum = 0;
+    for(int i = 0; i < s.size(); i++)
+        sum += s[i] - '0';
+    
+    return x % sum == 0 ? true : false;
+}
