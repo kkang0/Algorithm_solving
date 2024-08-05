324e74d51d67120fcb6aedeee7c2814ecfe9d8d3
Jeong yoon Kang <124678039+kkang0@users.noreply.github.com>
2024-08-06 00:10:05 +0900

    [level 2] Title: 구명보트, Time: 1.81 ms, Memory: 4.68 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.cpp
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int solution(vector<int> people, int limit) {
     int save = 0;
     int person = people.size() - 1;
-    
     int result = 0;
     sort(people.begin(), people.end());
+    
     while(save <= person) {
         if(people[save] + people[person] <= limit) save++;
         person--;
