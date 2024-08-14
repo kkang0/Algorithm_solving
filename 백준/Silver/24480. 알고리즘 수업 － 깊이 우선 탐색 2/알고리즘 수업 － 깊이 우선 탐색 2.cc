7d9d5d164cbd97a1bdbedefaa936563e1d9b57be
Jeong yoon Kang <124678039+kkang0@users.noreply.github.com>
2024-08-14 18:14:00 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 76 ms, Memory: 15420 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool check[200000];
+int Count[200000];
+vector<int> graph[200000];
+int visitOrder = 1;
+
+bool compare(int a, int b) {
+    if(a > b) return true;
+    return false;
+}
+void dfs(int start) {
+    if (check[start]) {
+        return;
+    }
+    check[start] = true;
+    Count[start] = visitOrder++;
+    
+    sort(graph[start].begin(), graph[start].end(), compare);
+
+    for (int i = 0; i < graph[start].size(); i++) {
+        int next = graph[start][i];
+        if (!check[next]) {
+            dfs(next);
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, m, First;
+    cin >> n >> m >> First;
+
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+
+    dfs(First);
+
+    for (int i = 1; i <= n; i++) {
+        cout << Count[i] << '\n';
+    }
+
+    return 0;
+}
