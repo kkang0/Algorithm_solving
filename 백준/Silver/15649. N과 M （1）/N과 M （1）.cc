0edf76cf8f99405a398b741f98e0d9836e55a8fe
Jeong yoon Kang <124678039+kkang0@users.noreply.github.com>
2025-01-23 23:06:12 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int N, M;
+int arr[9];
+bool visited[9];
+
+void dfs(int cnt) {
+    if(cnt == M) {
+        for(int i = 0; i < M; i++) cout << arr[i] << " ";
+        cout << "\n";
+        return;
+    }
+
+    for(int i = 1; i <= N; i++) {
+        if(!visited[i]) {
+            visited[i] = true;
+            arr[cnt] = i;
+            dfs(cnt + 1);
+            visited[i] = false;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> N >> M;
+    dfs(0);
+    return 0;
+}
