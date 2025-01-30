3d3e4a92d9b67c9b1743dc05d589baf15b363033
Jeong yoon Kang <124678039+kkang0@users.noreply.github.com>
2025-01-30 18:44:51 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,49 +1,47 @@
 #include <iostream>
-#include <vector>
 #include <cstring>
 using namespace std;
 
 int T, M, N, K;
-int graph[51][51];
+int arr[51][51];
 bool visited[51][51];
 int dx[4] = {1, -1, 0, 0};
 int dy[4] = {0, 0, 1, -1};
 
 void dfs(int x, int y) {
-    for(int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++) {
         int nx = x + dx[i];
         int ny = y + dy[i];
 
-        if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-        if(graph[nx][ny] == 1 && !visited[nx][ny]) {
-            visited[nx][ny] = true;
-            dfs(nx, ny);
-        }
+        if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+        if (visited[nx][ny] || arr[nx][ny] == 0) continue;
+        visited[nx][ny] = true;
+        dfs(nx, ny);
     }
 }
 
 int main(void) {
     cin >> T;
-    while(T--) {
+
+    for (int i = 0; i < T; i++) {
+        memset(arr, 0, sizeof(arr));
+        memset(visited, false, sizeof(visited));
+
         cin >> M >> N >> K;
 
-        // 초기화
-        memset(graph, 0, sizeof(graph));
-        memset(visited, false, sizeof(visited));
-        
-        int result = 0;
-        for(int j = 0; j < K; j++) {
+        for (int j = 0; j < K; j++) {
             int x, y;
             cin >> x >> y;
-            graph[y][x] = 1;
+            arr[y][x] = 1;
         }
 
-        for(int j = 0; j < N; j++) {
-            for(int k = 0; k < M; k++) {
-                if(graph[j][k] == 1 && !visited[j][k]) {
+        int result = 0;
+        for (int j = 0; j < N; j++) {
+            for (int k = 0; k < M; k++) {
+                if (!visited[j][k] && arr[j][k] == 1) {
                     visited[j][k] = true;
-                    result++;
                     dfs(j, k);
+                    result++;
                 }
             }
         }
