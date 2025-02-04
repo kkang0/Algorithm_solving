78e29c3c836c8ee874c89421fef50c0cc91d5e9c
Jeong yoon Kang <124678039+kkang0@users.noreply.github.com>
2025-02-04 22:43:29 +0900

    [level 0] Title: 배열 만들기 5, Time: 5.44 ms, Memory: 93.5 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/README.md
+++ 프로그래머스/0/181912. 배열 만들기 5/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 만들기 5 - 181912 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181912) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181912?language=java) 
 
 ### 성능 요약
 
-메모리: 6.7 MB, 시간: 2.44 ms
+메모리: 93.5 MB, 시간: 5.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 22일 12:08:58
+2025년 02월 04일 22:43:27
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> solution(String[] intStrs, int k, int s, int l) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        
+        for(int i = 0; i < intStrs.length; i++) {
+            String str = intStrs[i].substring(s, s + l);
+            int num = Integer.parseInt(str);
+            if(num > k) answer.add(num);
+        }
+        return answer;
+    }
+}
