20b45238f299c502a788ebbd82ee0cb365341725
Jeongyoon <124678039+kkang0@users.noreply.github.com>
2025-02-24 23:51:37 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[1001][1001];
+
+int main(void) {
+    string a, b;
+    cin >> a;
+    cin >> b;
+
+    for(int i = 1; i <= a.size(); i++) {
+        for(int j = 1; j <= b.size(); j++) {
+            if(a[i - 1] == b[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+            else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    
+    cout << dp[a.size()][b.size()] << endl;
+        
+    return 0;
+}
