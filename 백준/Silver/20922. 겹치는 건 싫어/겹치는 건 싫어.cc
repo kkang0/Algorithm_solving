a9d905b40eee9a3af13437fd35193483b3f6aeed
Jeongyoon <124678039+kkang0@users.noreply.github.com>
2025-02-27 16:20:33 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 92 ms, Memory: 7484 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.cc
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.cc
@@ -1,30 +1,28 @@
 #include <iostream>
-#include <vector>
 #include <unordered_map>
 #include <algorithm>
 using namespace std;
+
+int A[200001];
 
 int main(void) {
     int N, K;
     cin >> N >> K;
 
-    vector<int> v(N);
     for(int i = 0; i < N; i++) {
-        cin >> v[i];
+        cin >> A[i];
     }
 
     unordered_map<int, int> m;
     int left = 0;
     int result = 0;
     for(int right = 0; right < N; right++) {
-        m[v[right]]++;
+        m[A[right]]++;
 
-        while(!m.empty() && m[v[right]] > K) {
-            m[v[left]]--;
+        while(!m.empty() && m[A[right]] > K) {
+            m[A[left]]--;
 
-            if(m[v[left]] == 0) {
-                m.erase(v[left]);
-            }
+            if(m[A[left]] == 0) m.erase(A[left]);
             left++;
         }
 
