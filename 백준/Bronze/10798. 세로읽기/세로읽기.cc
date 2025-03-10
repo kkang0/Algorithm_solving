501fff32c44465f4e7a55713cc7638461c1f6b6a
Jeongyoon <124678039+kkang0@users.noreply.github.com>
2025-03-10 18:48:33 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+char arr[5][15];
+
+int main(void) {
+    for(int i = 0; i < 5; i++) {
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < 15; i++) {
+        for(int j = 0; j < 5; j++) {
+            if(arr[j][i] != NULL) cout << arr[j][i];
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
