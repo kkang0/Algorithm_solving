223a68f85f63719b2ed1e000e177a58363ec7a18
Jeongyoon <124678039+kkang0@users.noreply.github.com>
2025-04-28 00:47:01 +0900

    [Silver II] Title: 창고 다각형, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.cc
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.cc
@@ -1,7 +1,8 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
+
+int arr[1001];
 
 int main(void) {
     ios::sync_with_stdio(false);
@@ -10,45 +11,35 @@ int main(void) {
     int N;
     cin >> N;
 
-    vector<pair<int, int>> v;
-    for (int i = 0; i < N; i++) {
+    int max_h = 0;
+    int max_pos = 0;
+
+    for(int i = 0; i < N; i++) {
         int L, H;
         cin >> L >> H;
-        v.push_back({L, H});
+
+        arr[L] = H;
+
+        if(max_h < H) {
+            max_h = H;
+            max_pos = L;
+        }
     }
-
-    sort(v.begin(), v.end(), [](pair<int, int> a, pair<int, int> b) {
-        return a.first < b.first;
-    });
-
-    auto it = max_element(v.begin(), v.end(), [](pair<int, int> a, pair<int, int> b) {
-        return a.second < b.second;
-    });
-    int idx = distance(v.begin(), it);
 
     int result = 0;
-
-    int max_h = v[0].second;
-    int pos = v[0].first;
-    for (int i = 1; i <= idx; i++) {
-        if (v[i].second >= max_h) {
-            result += (v[i].first - pos) * max_h;
-            max_h = v[i].second;
-            pos = v[i].first;
-        }
+    int lh = 0;
+    for(int i = 1; i < max_pos; i++) {
+        lh = max(lh, arr[i]);
+        result += lh;
     }
 
-    max_h = v.back().second;
-    pos = v.back().first;
-    for (int i = N - 2; i >= idx; i--) {
-        if (v[i].second >= max_h) {
-            result += (pos - v[i].first) * max_h;
-            max_h = v[i].second;
-            pos = v[i].first;
-        }
+    int rh = 0;
+    for(int i = 1000; i > max_pos; i--) {
+        rh = max(rh, arr[i]);
+        result += rh;
     }
 
-    result += v[idx].second;
+    result += max_h;
 
     cout << result << '\n';
 
