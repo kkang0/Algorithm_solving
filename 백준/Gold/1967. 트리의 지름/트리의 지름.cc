0b389c599d955f9670d9170c97dc34038fffa62f
Jeongyoon <124678039+kkang0@users.noreply.github.com>
2025-05-12 16:12:43 +0900

    [Gold IV] Title: 트리의 지름, Time: 4 ms, Memory: 3004 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.cc
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.cc
@@ -1,49 +1,50 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
+int n;
 vector<pair<int, int>> tree[10001];
 bool visited[10001];
-int max_num = 0;
+int max_cost = 0;
 int max_node = 0;
 
-void dfs(int n, int d) {
-    visited[n] = true;
+void dfs(int node, int cost) {
+    visited[node] = true;
 
-    if(max_num < d) {
-        max_num = d;
-        max_node = n;
+    if(max_cost < cost) {
+        max_cost = cost;
+        max_node = node;
     }
 
-    for(int i = 0; i < tree[n].size(); i++) {
-        int next = tree[n][i].first;
-        int weight = tree[n][i].second;
+    for(int i = 0; i < tree[node].size(); i++) {
+        int next = tree[node][i].first;
+        int nextCost = tree[node][i].second;
 
         if(!visited[next]) {
-            dfs(next, d + weight);
+            dfs(next, cost + nextCost);
         }
     }
 }
 
 int main(void) {
-    int n;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     cin >> n;
 
     for(int i = 0; i < n - 1; i++) {
-        int u, v, d;
-        cin >> u >> v >> d;
+        int from ,to, cost;
+        cin >> from >> to >> cost;
 
-        tree[u].push_back({v, d});
-        tree[v].push_back({u, d});
+        tree[from].push_back({to, cost});
+        tree[to].push_back({from, cost});
     }
 
     dfs(1, 0);
     fill(visited, visited + n + 1, false);
-    max_num = 0;
-    dfs(max_node, max_num);
+    dfs(max_node, 0);
 
-    cout << max_num << endl;
+    cout << max_cost << "\n";
 
     return 0;
 }
