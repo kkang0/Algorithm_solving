38747dbad746afc4170fed2b37877a59d7007993
Jeongyoon <124678039+kkang0@users.noreply.github.com>
2025-05-19 18:09:06 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -2,31 +2,32 @@
 #include <algorithm>
 using namespace std;
 
-int N;
-int color[1001][1001];
-int dp[1001][1001];
+int color[1001][3];
+int dp[1001][3];
 
 int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
     cin >> N;
 
     for(int i = 0; i < N; i++) {
-        for(int j = 0; j < 3; j++) {
-            cin >> color[i][j];
-        }
+        cin >> color[i][0] >> color[i][1] >> color[i][2];
     }
 
     dp[0][0] = color[0][0];
     dp[0][1] = color[0][1];
     dp[0][2] = color[0][2];
 
-    int sum = 0;
+    int result = 0;
     for(int i = 1; i < N; i++) {
         dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + color[i][0];
         dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + color[i][1];
         dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + color[i][2];
     }
 
-    cout << min({dp[N - 1][0], dp[N - 1][1], dp[N - 1][2]}) << endl;
+    cout << min({dp[N - 1][0], dp[N - 1][1], dp[N - 1][2]}) << "\n";
 
     return 0;
 }
