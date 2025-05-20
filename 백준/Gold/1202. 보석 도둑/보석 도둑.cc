f0be53d62cf0f7596438a8a14a36638960a50ac2
Jeongyoon <124678039+kkang0@users.noreply.github.com>
2025-05-20 23:29:46 +0900

    [Gold II] Title: 보석 도둑, Time: 156 ms, Memory: 8740 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -1,12 +1,46 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, K;
+    cin >> N >> K;
+
+    vector<pair<int, int>> jewel(N);
+    for(int i = 0; i < N; i++) {
+        cin >> jewel[i].first >> jewel[i].second;
+    }
+
+    sort(jewel.begin(), jewel.end());
+
+    vector<int> bag(K);
+    for(int i = 0; i < K; i++) {
+        cin >> bag[i];
+    }
+
+    sort(bag.begin(), bag.end());
+
+    priority_queue<int, vector<int>, less<int>> pq;
+    long long result = 0;
+    int idx = 0;
+    for(int i = 0; i < K; i++) {
+        while(idx < N && bag[i] >= jewel[idx].first) {
+            pq.push(jewel[idx].second);
+            idx++;
+        }
+
+        if(!pq.empty()) {
+            result += pq.top();
+            pq.pop();
+        }
+    }
+
+    cout << result << "\n";
+
+    return 0;
+}
