8b145795b28266f0d724bb0a3fb4b9357167bf56
Jeongyoon <124678039+kkang0@users.noreply.github.com>
2025-05-20 18:45:36 +0900

    [Gold II] Title: 보석 도둑, Time: 420 ms, Memory: 8496 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -1,43 +1,12 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-using namespace std;
-
-int main(void) {
-    int N, K;
-    cin >> N >> K;
-
-    pair<int, int> jewel[300001];
-    for(int i = 0; i < N; i++) {
-        cin >> jewel[i].first >> jewel[i].second;
-    }
-    
-    int bag[300001];
-    priority_queue<int, vector<int>, less<int>> pq;
-
-    for(int i = 0; i < K; i++) {
-        cin >> bag[i];
-    }
-
-    sort(jewel, jewel + N);
-    sort(bag, bag + K);
-
-    int idx = 0;
-    long long sum = 0;
-    for(int i = 0; i < K; i++) {
-        while(idx < N && bag[i] >= jewel[idx].first) {
-            pq.push(jewel[idx].second);
-            idx++;
-        }
-
-        if(!pq.empty()) {
-            sum += pq.top();
-            pq.pop();
-        }
-    }
-
-    cout << sum << endl;
-
-    return 0;
-}
+<html>
+<head><title>502 Bad Gateway</title></head>
+<body>
+<center><h1>502 Bad Gateway</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
