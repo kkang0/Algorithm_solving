5119091709f0eccd34735b5d2157c04db21b714f
Jeongyoon <124678039+kkang0@users.noreply.github.com>
2025-06-20 20:54:22 +0900

    [Gold V] Title: 노드사이의 거리, Time: 4 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
@@ -2,50 +2,47 @@
 #include <vector>
 using namespace std;
 
+int N, M;
 vector<pair<int, int>> graph[1001];
 bool visited[1001];
-int cnt = 0;
-int result = 0;
+int result;
 
-void dfs(int cur, int dest) {
+void dfs(int cur, int end, int dist) {
+    if (cur == end) {
+        result = dist;
+        return;
+    }
+
     visited[cur] = true;
 
-    for (pair<int, int> child : graph[cur]) {
-        if (child.first == dest) {
-            result = cnt + child.second;
-            return;
-        }
-        if (!visited[child.first]) {
-            cnt += child.second;
-            dfs(child.first, dest);
-            cnt -= child.second;
+    for (auto [w, next] : graph[cur]) {
+        if (!visited[next]) {
+            dfs(next, end, dist + w);
+            if (result != -1) return;
         }
     }
 }
 
 int main(void) {
-    int N, M;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     cin >> N >> M;
-
     for (int i = 0; i < N - 1; i++) {
-        int u, v, e;
-        cin >> u >> v >> e;
-
-        graph[u].push_back({v, e});
-        graph[v].push_back({u, e});
+        int u, v, d;
+        cin >> u >> v >> d;
+        graph[u].emplace_back(d, v);
+        graph[v].emplace_back(d, u);
     }
 
     for (int i = 0; i < M; i++) {
-        int u, v;
-        cin >> u >> v;
+        int s, e;
+        cin >> s >> e;
+        fill(visited, visited + N + 1, false);
+        result = -1;
 
-        fill(visited, visited + 1001, false);
-        cnt = 0;
-        result = 0;
-
-        dfs(u, v);
-
-        cout << result << endl;
+        dfs(s, e, 0);
+        cout << result << '\n';
     }
 
     return 0;
