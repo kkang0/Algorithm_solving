6e75b5d42265f1eed266a35776fa4ebcb6ee84f9
Jeongyoon <124678039+kkang0@users.noreply.github.com>
2025-07-03 17:17:53 +0900

    [Silver III] Title: N과 M (7), Time: 424 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int nums[8];
+int tmp[8];
+
+void dfs(int cnt) {
+    if(cnt == M) {
+        for(int i = 0; i < cnt; i++) cout << tmp[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    for(int i = 0; i < N; i++) {
+        tmp[cnt] = nums[i];
+        dfs(cnt + 1);
+    }
+}
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin >> N >> M;
+    for(int i = 0; i < N; i++) {
+        cin >> nums[i];
+    }
+
+    sort(nums, nums + N);
+    dfs(0);
+
+    return 0;
+}
