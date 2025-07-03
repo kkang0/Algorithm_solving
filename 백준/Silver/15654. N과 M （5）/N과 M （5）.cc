7dd4f760a204c1be0c9228f02421bf53f67b82c1
Jeongyoon <124678039+kkang0@users.noreply.github.com>
2025-07-03 17:09:04 +0900

    [Silver III] Title: N과 M (5), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -3,36 +3,38 @@
 using namespace std;
 
 int N, M;
-int arr[9];
-int result[9];
-int visited[9];
+int nums[9];
+int tmp[9];
+bool visited[9];
 
-void dfs(int cnt) {
+void dfs(int idx, int cnt) {
     if(cnt == M) {
-        for(int i = 0; i < M; i++) cout << result[i] << " ";
-        cout << "\n";
-        return;
+        for(int i = 0; i < cnt; i++) cout << tmp[i] << ' ';
+        cout << '\n';
     }
 
     for(int i = 0; i < N; i++) {
         if(!visited[i]) {
             visited[i] = true;
-            result[cnt] = arr[i];
-            dfs(cnt + 1);
+            tmp[cnt] = nums[i];
+            dfs(i + 1, cnt + 1);
             visited[i] = false;
         }
     }
 }
 
 int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     cin >> N >> M;
 
     for(int i = 0; i < N; i++) {
-        cin >> arr[i];
+        cin >> nums[i];
     }
 
-    sort(arr, arr + N);
-    dfs(0);
+    sort(nums, nums + N);
+    dfs(0, 0);
 
     return 0;
 }
