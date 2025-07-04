75caa12ba98e1e54091c261043f570c1d132425a
Jeongyoon <124678039+kkang0@users.noreply.github.com>
2025-07-04 22:55:58 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 216 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -1,25 +1,27 @@
 #include <iostream>
 using namespace std;
 
-void print(int x, int y, int n) {
-    if((x / n) % 3 == 1 && (y / n) % 3 == 1) {
-        cout << ' ';
-    } else {
+int N;
+
+void star(int x, int y, int n) {
+    if((x / n) % 3 == 1 && (y / n) % 3 == 1) cout << ' ';
+    else {
         if(n == 1) cout << '*';
-        else print(x, y, n / 3);
+        else star(x, y, n / 3);
     }
 }
 
-
 int main(void) {
-    int N;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     cin >> N;
 
     for(int i = 0; i < N; i++) {
         for(int j = 0; j < N; j++) {
-            print(i, j, N);
+            star(i, j, N);
         }
-        cout << "\n";
+        cout << '\n';
     }
 
     return 0;
