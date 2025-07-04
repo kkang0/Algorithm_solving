b004f39e8a8c95887b37e4b980bc2f0e5fe7668f
Jeongyoon <124678039+kkang0@users.noreply.github.com>
2025-07-04 15:17:14 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int nums[9];
+int tmp[9];
+bool visited[9];
+
+void dfs(int idx, int cnt) {
+    if(cnt == M) {
+        for(int i = 0; i < cnt; i++) cout << tmp[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    int before = -1;
+    for(int i = idx; i < N; i++) {
+        if(visited[i]) continue;
+        if(nums[i] == before) continue;
+
+        visited[i] = true;
+        before = nums[i];
+        tmp[cnt] = nums[i];
+        dfs(i + 1, cnt + 1);
+        visited[i] = false;
+    }
+}
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> N >> M;
+    for(int i = 0; i < N; i++) {
+        cin >> nums[i];
+    }
+
+    sort(nums, nums + N);
+    dfs(0, 0);
+
+    return 0;
+}
