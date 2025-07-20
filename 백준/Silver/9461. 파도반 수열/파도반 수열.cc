e052ff990e64122ab8e531a6142c7f0f7739cd1e
Jeongyoon <124678039+kkang0@users.noreply.github.com>
2025-07-21 00:00:27 +0900

    [Silver III] Title: 파도반 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.cc
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -11,10 +11,9 @@ int main(void) {
     dp[2] = 1;
     dp[3] = 1;
     dp[4] = 2;
-    dp[5] = 2;
 
-    for(int i = 6; i <= 100; i++) {
-        dp[i] = dp[i - 5] + dp[i - 1];
+    for(int i = 5; i <= 100; i++) {
+        dp[i] = dp[i - 5] + dp[i - 4] + dp[i - 3];
     }
 
     int T;
