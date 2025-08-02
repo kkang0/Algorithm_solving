6ee3b0cb116370c2c4a7f5c748a0872e21204c73
Jeongyoon <124678039+kkang0@users.noreply.github.com>
2025-08-02 20:05:25 +0900

    [Silver I] Title: 동물원, Time: 8 ms, Memory: 7532 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    vector<vector<int>> dp(N + 1, vector<int>(3));
+    dp[1][0] = 1;
+    dp[1][1] = 1;
+    dp[1][2] = 1;
+
+    for(int i = 2; i <= N; i++) {
+        dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % 9901;
+        dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % 9901;
+        dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % 9901;
+    }
+
+    cout << (dp[N][0] + dp[N][1] + dp[N][2]) % 9901 << '\n';
+
+    return 0;
+}
