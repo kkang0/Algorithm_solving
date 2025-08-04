dadc1eebcda26a8db167354bf361dec040825dfe
Jeongyoon <124678039+kkang0@users.noreply.github.com>
2025-08-04 14:33:54 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.cc
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -1,27 +1,29 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-int n;
-int v[10001];
-int dp[10001];
-
 int main(void) {
-    cin >> n;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    for(int i = 1; i <= n; i++){
-        cin >> v[i];
+    int n;
+    cin >> n;
+    vector<int> arr(n + 1);
+    vector<int> dp(n + 1);
+    for(int i = 1; i <= n; i++) {
+        cin >> arr[i];
     }
 
     dp[0] = 0;
-    dp[1] = v[1];
-    dp[2] = v[1] + v[2];
+    dp[1] = arr[1];
+    dp[2] = arr[1] + arr[2];
 
     for(int i = 3; i <= n; i++) {
-        dp[i] = max({dp[i - 3] + v[i - 1] + v[i], dp[i - 2] + v[i], dp[i - 1]});
+        dp[i] = max({dp[i - 1], dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]});
     }
 
-    cout << dp[n] << endl;
+    cout << dp[n] << '\n';
 
     return 0;
 }
