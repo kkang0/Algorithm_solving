442853a0de82261116b5819830bd60deb13d65f2
Jeongyoon <124678039+kkang0@users.noreply.github.com>
2025-09-23 23:24:38 +0900

    [Silver III] Title: 01타일, Time: 8 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+    vector<int> dp(N + 1, 1);
+
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for(int i = 3; i <= N; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+    }
+
+    cout << dp[N] << '\n';
+
+    return 0;
+}
